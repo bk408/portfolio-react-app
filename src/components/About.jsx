48f76ba8feca5217214c2b46ce72d79491a6be38
bk408 <bhavyakhatri1912@gmail.com>
2023-02-13 16:27:19 +0530

    update height of screen

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white">
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+    <div name="about" className="w-full md:h-screen bg-gradient-to-b from-gray-800 to-black text-white">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col   justify-center w-full h-full ">
         <div className="pb-8">
-            <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+            <p className="text-4xl  font-bold inline border-b-4 border-gray-500">About</p>
         </div>
-        <p className="text-xl mt-20">Hi, my name is Bhavya Khatri and I am a front-end developer with a strong passion for creating intuitive and visually appealing websites. I have a solid understanding of HTML, CSS, and JavaScript, and I am eager to apply my skills to real-world projects.
+        <p className="text-xl  mt-20 ">Hi, my name is Bhavya Khatri and I am a front-end developer with a strong passion for creating intuitive and visually appealing websites. I have a solid understanding of HTML, CSS, and JavaScript, and I am eager to apply my skills to real-world projects.
 
 During my studies, I completed several projects that required me to design, build, and test websites from scratch. This experience has given me a strong foundation in front-end development and has allowed me to develop my skills in a practical setting. I am also familiar with React and I am excited to continue exploring this technology to build dynamic user interfaces.</p>
       
@@ -15,7 +15,7 @@ During my studies, I completed several projects that required me to design, buil
       <br />
 
 
-      <p className="text-xl">As a front-end developer, my goal is to create websites that not only look great, but also provide a seamless user experience. I am constantly seeking new challenges and opportunities to grow, and I believe that my attention to detail, passion for technology, and drive to succeed will make me a valuable asset to any team.
+      <p className="text-xl  ">As a front-end developer, my goal is to create websites that not only look great, but also provide a seamless user experience. I am constantly seeking new challenges and opportunities to grow, and I believe that my attention to detail, passion for technology, and drive to succeed will make me a valuable asset to any team.
 
 When I'm not coding, I enjoy staying up-to-date with the latest industry trends, experimenting with new technologies, and exploring my creative side through design and photography.
 
