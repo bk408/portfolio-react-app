4413b4a42eb15c51d835f99bbc30de17227232eb
bk408 <bhavyakhatri1912@gmail.com>
2023-02-13 17:42:02 +0530

    navbar title update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
           </div>
         </div>
         <div>
-          <img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full  " />
+          <img src={HeroImage} alt="my profile" className="rounded-2xl mx-auto w-2/3 md:w-full" />
         </div>
       </div>
     </div>
